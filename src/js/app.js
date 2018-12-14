67c9cab753fc8a303f8cf27492ce1d088159398c
Nicholas Cannon <nicholascannon1@gmail.com>
2018-12-14 17:03:40 +0800

    Stable single page layout and single page navbar

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ const $ = require('jquery');
 require('popper.js');
 require('bootstrap/dist/js/bootstrap.bundle.min');
 require('bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap/js/dist/util')
+require('bootstrap/js/dist/scrollspy');
 
 // My Css files
 require('../css/style.css');
@@ -13,18 +15,57 @@ require('../css/skills.css');
 
 const logos = ['javascript', 'nodejs', 'python', 'mysql', 'mongodb', 'html5', 'css3', 'react'];
 
+/**
+ * Detects if an element is currently in the viewport
+ */
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+function navClick(id) {
+  // Scroll the element into the viewport
+  document.getElementById(id).scrollIntoView({
+    behavior: 'smooth', block: 'start'
+  });
+
+  /*
+  // Highlight nav-link in navbar
+  $('#'+id+'Link').addClass('active');
+  */
+}
+
 $('document').ready(() => {
+  //$('body').scrollspy({target: ".navbar"})
+
   // Add event listeners to navigation buttons
   // return false to stop page from refreshing
-  $('#brand').click(function() { return false; });
-  $('#pageLink1').click(function() { return false; });
-  $('#pageLink2').click(function() { return false; });
-  $('#pageLink3').click(function() { return false; });
-  $('#pageLink4').click(function() { return false; });
-  $('#landingBtn').click(function() { });
+  $('#landingLink').click(function() { navClick('landing'); return false; });
+  $('#aboutLink').click(function() { navClick('about'); return false; });
+  $('#projectsLink').click(function() { navClick('projects'); return false; });
+  $('#skillsLink').click(function() { navClick('skills'); return false; });
+  $('#contactLink').click(function() { navClick('contact'); return false; });
+  $('#landingBtn').click(function() { navClick('projects'); return false;});
 
+  /**
+   * Window resize and scroll function. This function is used to 
+   * remove the navbar on the landing screen.
+   */
+  $(window).on('resize scroll', function() {
+    if ($('#landing').isInViewport()) {
+      $('nav').removeClass('showNav').addClass('hideNav');
+    } else {
+      $('nav').addClass('showNav').removeClass('hideNav');
+    }
+  });
+
+  /*
   // Load SVG files
   logos.forEach(logo => {
     $(`#${logo}Logo`).attr('src', require(`../imgs/logos/${logo}.svg`));
   });
+  */
 });
