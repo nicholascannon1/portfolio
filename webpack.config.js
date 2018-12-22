6a06ee5ae9813ca38d197e957d745c115544d665
Nicholas Cannon <nicholascannon1@gmail.com>
2018-12-22 16:27:12 +0800

    Added pdf resume and update webpack config to process static pdf files

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         limit: 8000, // converts images under 8kb to base64 strings
         name: 'images/[hash]-[name].[ext]'
       } },
-      { test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader', options: {
+      { test: /\.(woff(2)?|ttf|eot|pdf)(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader', options: {
         name: '[name].[ext]',
         outputPath: 'fonts/'
       } }
